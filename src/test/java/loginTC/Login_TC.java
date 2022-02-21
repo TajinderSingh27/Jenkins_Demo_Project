8046023408c4041c06f21d69246312995a02984a
Tajinder <Tajinderpals@ymail.com>
2022-02-21 23:22:44 +0530

    Added Extent Reporting

--- src/test/java/loginTC/Login_TC.java
+++ src/test/java/loginTC/Login_TC.java
@@ -4,17 +4,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Login_TC {
+	private ExtentReports extent=new ExtentReports();
+	private ExtentSparkReporter spark =new ExtentSparkReporter("extent.html");
+	private WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	extent.attachReporter(spark);
+	System.out.println(driver);
+	}
 
 	@Test
 	public void login() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		ExtentTest test=extent.createTest("*****Login Functionality*****").createNode("first").assignAuthor("Tajinder Singh").assignCategory("Regression").assignDevice("Windows");
 		
 		driver.get("https://opensource-demo.orangehrmlive.com/"); 
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin" ) ; 
@@ -22,6 +41,15 @@ public class Login_TC {
 		driver.findElement(By.id("btnLogin")).click() ; 
 		Assert.assertEquals(driver.getTitle() , "OrangeHRM");
 		System.out.println("Test scenario got passed successfully.");
+		test.log(Status.PASS, "Login functioality verified");
+		test.pass("Passed");
 		driver.close(); 	
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+	extent.flush();
+	if(driver!=null)
+	driver.quit();
 	}
 }
